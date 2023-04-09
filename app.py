5b503506b2220e7a2913e574b7299493af4a94c7
mohamed madobe <mohamedmadobe@mohameds-MacBook-Pro.local>
2023-04-09 15:34:07 -0500

    exercise

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ihaveasecret'
+
+toolbar = DebugToolbarExtension(app)
+
+
+connect_db(app)
+db.create_all()
